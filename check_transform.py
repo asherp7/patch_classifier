40a08e677eeb91f198861e69929c2f869c40c776
Asher Patinkin <asherp7@cs.huji.ac.il>
2020-01-15 15:29:54 +0200

    transform2h5.py seems to work.

--- check_transform.py
+++ check_transform.py
@@ -4,12 +4,19 @@ import numpy as np
 import nibabel as nib
 
 nifti_dir_path = '/cs/labs/josko/aszeskin/Rafi_Tumor_data/allBL'
+roi_dir_path = '/cs/labs/josko/aszeskin/Rafi_Tumor_data/allBL_liverSeg'
+tumor_dir_path = '/cs/labs/josko/aszeskin/Rafi_Tumor_data/allBL_onlytumors'
 output_path = '/mnt/local/aszeskin/asher/transforfm_h5_output'
+roi_suffix = '_liverseg'
+tumor_suffix = '_Tumors'
 patch_size = 35
 overlapping = 3
-transform = Transform2h5(nifti_dir_path, output_path, ('L', 'P', 'S'), patch_size, overlapping)
-nifti = transform.read_nifti('BL01.nii.gz')
-np.set_printoptions(precision=2, suppress=True)
+transform = Transform2h5(nifti_dir_path, output_path, ('L', 'P', 'S'), patch_size, overlapping, roi_dir_path,
+                         roi_suffix, tumor_dir_path, tumor_suffix)
+transform.save_all_nifti_patches()
+
+# nifti = transform.read_nifti('/cs/labs/josko/aszeskin/Rafi_Tumor_data/allBL/'+'BL01.nii.gz')
+# np.set_printoptions(precision=2, suppress=True)
 # print(nifti.affine)
 # print(nib.aff2axcodes(nifti.affine))
 # print('\n\n')
@@ -19,11 +26,11 @@ np.set_printoptions(precision=2, suppress=True)
 
 # transform.save_all_nifti_patches()
 
-nifti_list = transform.get_nifti_filenames()
-for filename in nifti_list:
-    nifti = transform.read_nifti(filename)
-    canonical_img = nib.as_closest_canonical(nifti)
-    print(filename, nib.aff2axcodes(nifti.affine), nib.aff2axcodes(canonical_img.affine))
+# nifti_list = transform.get_nifti_filenames()
+# for filename in nifti_list:
+#     nifti = transform.read_nifti(filename)
+#     canonical_img = nib.as_closest_canonical(nifti)
+#     print(filename, nib.aff2axcodes(nifti.affine), nib.aff2axcodes(canonical_img.affine))
 
 # print('processing...')
 # patches = transform.save_single_nifti_patches(nifti)
@@ -39,6 +46,13 @@ for filename in nifti_list:
 # hdr = img.header
 # hdr.get_xyzt_units()
 
-arr = np.zeros([100,100,100])
-arr[10:90,20:80,30:70] = 1
-transform.get_coarse_roi_bbox(arr)
+# arr = np.zeros([100,100,100])
+# arr[10:90,20:80,30:70] = 1
+# transform.get_coarse_roi_bbox(arr)
+
+f = h5py.File('/mnt/local/aszeskin/asher/transforfm_h5_output/patches.h5', 'r')
+print(f.keys())
+print(f['patches'])
+print(f['labels'])
+print(f['tumor_idx'])
+print(f['non_tumor_idx'])
